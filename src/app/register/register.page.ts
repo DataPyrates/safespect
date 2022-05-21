00a461bda2ff9f300f92b7dddd48d3e429579826
Taruna Tiwari <tiwaritaruna97@gmail.com>
2022-05-21 18:21:47 +0200

    ui

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './../api/user.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  username: any;
+  phone: any;
+  email: any;
+  password: any;
+ 
+  constructor(private route: Router, private api: UserService) { }
 
   ngOnInit() {
   }
 
+  register(){
+    if(this.username !=undefined && this.phone !=undefined && this.email !=undefined 
+      && this.password !=undefined){
+      this.api.register(this.username,this.phone,this.email,this.password).subscribe(
+      data => {
+      console.log(data);
+       if((data[0]['status']=='success')){
+         alert('Regsiter Successfully !!!');
+         this.route.navigate(['/login']);
+       }
+       else{
+        alert(data[0]['msg']);
+       }
+      })
+      
+    }
+    else{
+      alert("Please Enter all the details");
+    }
+  }
+
+  gotoLogin(){
+    this.route.navigate(['/login']);
+  }
+  
 }
